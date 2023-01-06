196a19bfcbaa03322e8144d35b193fec48ac2e39
Ruslan Svirskey <ruslansvirskey@yandex.ru>
2023-01-06 21:50:43 +0300

    added gtest and cmake

--- Makefile
+++ Makefile
@@ -1,8 +1,28 @@
-all: 
+# all: 
+# 	mkdir -p build
+# 	cd build && cmake -G "Unix Makefiles" ..
+# 	cd build && cmake --build . 
+
+all: test
+
+test: run_test
+
+build_test:
 	mkdir -p build
-	cd build
-	cmake -G "MinGW Makefiles" ..
-	
+	cmake -S . -B build
+	cmake --build build
+
+run_test: build_test
+	cd build && ctest
+
+#main:
+#	g++ -std=c++20 main.cpp && ./a.out
+
+clean:
+	rm -rf ./build
+	rm -f a.out
+	rm -f *.o
+	rm -f output.txt
 
 # test: all
 # TODO : current all rule change to test, all rule can read from terminal
--- gtest.cpp
+++ gtest.cpp
@@ -1,10 +1,78 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
+	#include <gtest/gtest.h>
+	#include <iostream>
+	#include <fstream>
+	#include "sudoku.hpp"
 
-int main(int argc, char **argv)
-{
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
-  
-  return RUN_ALL_TESTS();
-}
+	TEST(Correct_9x9_common, no_need_recursion) {
+	std::ifstream input_fin("../test_data/simple.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/simple.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::solved_no_recursion);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
+	TEST(Correct_9x9_common, need_recursion_1) {
+	std::ifstream input_fin("../test_data/hard.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/hard.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::solved_with_recursion);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
+	TEST(Correct_9x9_common, need_recursion_2) {
+	std::ifstream input_fin("../test_data/crazy.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/crazy.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::solved_with_recursion);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
+	TEST(Valid_9x9_common, empty) {
+	std::ifstream input_fin("../test_data/empty.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/empty.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::solved_with_recursion);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
+
+	TEST(Invalid_9x9_common, duplicate_numbers) {
+	std::ifstream input_fin("../test_data/duplicates.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/duplicates.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::invalid);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
+	TEST(Invalid_9x9_common, invalid_nums) {
+	std::ifstream input_fin("../test_data/invalid_nums.txt");
+	svirskey::sudoku_solver solver(input_fin);
+	svirskey::sudoku_field result_field = solver.solve();
+
+	std::ifstream expected_fin("../expected_data/invalid_nums.txt");
+	svirskey::sudoku_field expected_field(expected_fin);
+
+	EXPECT_EQ(solver.get_status(), svirskey::solve_status::invalid);
+	EXPECT_EQ(result_field, expected_field);
+	}
+
